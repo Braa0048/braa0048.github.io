0dd6aa56f662b19bc74d9194cf1b4c2cc85974b8
Braa0048 <braa0048@hz.nl>
2023-10-11 11:03:09 +0200

    commit the site

---
+++ js/sidebar.js
@@ -0,0 +1,8 @@
+function openNav() {
+    console.log("test123");
+  document.getElementById("navbar-span").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
